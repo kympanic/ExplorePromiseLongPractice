40b3bb0a9a240f72726f131f9d9f091003475ba9
Daniel Yoo <dacapo0509@gmail.com>
2022-06-07 21:42:17 -0700

    phase1-3done

--- promises.js
+++ promises.js
@@ -1,51 +1,57 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-// Your code here
+// function num1() {
+// 	return 1;
+// }
+// async function num2() {
+// 	return "x";
+// }
 
-
+// console.log("num1", num1());
+// num2().then((res) => console.log(res));
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
-// Your code here
+// async function waiting() {
+// 	const value = await num2();
+// 	console.log("waiting", value);
+// }
 
-
+// waiting();
 
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-// Your code here
+async function waitingForGodot() {
+	const promise = new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("You just missed him!");
+		}, 10000);
+	});
 
-
-
+	const result = await promise;
+	console.log("Oh no!", result);
+}
+waitingForGodot();
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
-
-// Your code here
-
-
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
 // Your code here
 
-
-
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
 
 // Your code here
 
-
-
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
 // Your code here
-
-
 
 /* ============================== Phase 8 ============================== */
 /* -------------------- Promises are asynchronous! --------------------- */
