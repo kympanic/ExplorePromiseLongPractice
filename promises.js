4c2c15fc621ec305897df2c9b2e35f8604027db7
Alex Hiller <asonnyhiller@gmail.com>
2022-06-08 00:59:07 -0400

    ERRORS

--- promises.js
+++ promises.js
@@ -24,29 +24,61 @@
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-async function waitingForGodot() {
-	const promise = new Promise((resolve) => {
-		setTimeout(() => {
-			resolve("You just missed him!");
-		}, 10000);
-	});
+// async function waitingForGodot() {
+// 	const promise = new Promise((resolve) => {
+// 		setTimeout(() => {
+// 			resolve("You just missed him!");
+// 		}, 10000);
+// 	});
 
-	const result = await promise;
-	console.log("Oh no!", result);
-}
-waitingForGodot();
+// 	const result = await promise;
+// 	console.log("Oh no!", result);
+// }
+// waitingForGodot();
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
+    // new Promise((resolve) => {
+    //     setTimeout(() => {
+    //         resolve("Whatever we want.");
+    //     }, 2438);
+    // }).then(anything => console.log("Whatever YOU want.", anything));
+
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
-// Your code here
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+async function huh() {
+    await wait(2499)
+    console.log("Any message you want.")
+}
+
+huh();
 
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
 
-// Your code here
+function monogamy(randVal){
+    new Promise((resolve, reject) => {
+        if(randVal > .5){
+            resolve("happily ever after");
+        } else {
+            reject("til death do us part *cocks shotgun*");
+        }
+    });
+}
+
+for (let i = 1; i < 10; i++){
+    const rand = Math.random();
+    wait(2000 + rand*1000).then(() => monogamy(rand)).then((result) => {
+            console.log(`${i}th marriage: ${result}`);
+        }).catch(err => console.err(`Error: Marriage Failed`, err))
+};
 
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
